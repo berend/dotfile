62e1a54eaa7f18ce23d7032c3cb1daa55d9083a5
berend <berend.kapelle@gmail.com>
2017-03-29 22:44:27 +0200

    added system setup

---
+++ systemsetup.sh
@@ -0,0 +1,13 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install git
+brew cask install pycharm
+brew cask install spotify
+brew cask install spectales
+brew cask install sublime-text
+brew cask install telegram-desktop
+brew cask install iterm2
+brew cask install macdown
+brew install pyenv
+brew install pyenv-virtualenv
+pyenv install 3.6.1
+pyenv install 2.7.13
