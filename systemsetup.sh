96a1839f795bb1d68527d255d51f89d95e9c58d3
berend <berend.kapelle@gmail.com>
2017-05-09 17:03:34 +0200

    added jq

--- systemsetup.sh
+++ systemsetup.sh
@@ -1,8 +1,18 @@
+# brew initial setup
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
 brew upgrade
+
+# brew stuff
 brew install git
 brew install coreutils
+brew install jq
+brew install pyenv
+brew install pyenv-virtualenv
+brew install zsh
+brew install node
+
+# cask stuff
 brew cask install pycharm
 brew cask install spotify
 brew cask install spectales
@@ -13,12 +23,12 @@ brew cask install macdown
 brew cask install google-chrome
 brew cask install p4merge
 
-brew install pyenv
-brew install pyenv-virtualenv
-brew install zsh
+# remove unused stuff after all those installs
+brew cleanup
+
+# install common python version
 pyenv install 3.6.1
 pyenv install 2.7.13
 
+# zsh setup
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
-
-brew cleanup
